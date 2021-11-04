b6b7e0cb7f75eac3a257daeb5783bf26c1f21273
Abhinav110897 <abhinavsrivastava10897@gmail.com>
2021-11-04 15:12:27 +0530

    Life Cycle demo

--- src/main/java/guru/springframework/sfgdi/services/I18nEnglishGreetingService.java
+++ src/main/java/guru/springframework/sfgdi/services/I18nEnglishGreetingService.java
@@ -1,5 +1,6 @@
 package guru.springframework.sfgdi.services;
 
+import guru.springframework.sfgdi.repositories.EnglishGreetingRepository;
 
 /**
  * Created by jt on 12/27/19.
@@ -7,6 +8,12 @@ package guru.springframework.sfgdi.services;
 //@Profile("EN") Using config class instead
 //@Service("i18nService") Using config class instead
 public class I18nEnglishGreetingService implements GreetingService {
+
+    private final EnglishGreetingRepository englishGreetingRepository;
+    public I18nEnglishGreetingService(EnglishGreetingRepository englishGreetingRepository) {
+        this.englishGreetingRepository = englishGreetingRepository;
+    }
+
     @Override
     public String sayGreeting() {
         return "Hello World - EN";
